7e06429968dd78421dbdbd65a99ef1cf7154c94c
Ricky <plastic.acolyte.art@gmail.com>
2024-06-16 20:59:14 -0500

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,6 +1,17 @@
 from sys import argv
+from stack import Stack
 
-if __name__ == '__main__':
+
+def main():
+    memory = [Stack() for _ in range(777)]
+    stack_pointer = 0
+    active_chip = None
+
     with open(argv[1]) as file:
         for line in file:
             print(line.strip())
+
+
+if __name__ == '__main__':
+    main()
+
