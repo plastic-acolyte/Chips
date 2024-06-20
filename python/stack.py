27eeb41301e0c21943daf162f5a382cfe036802a
Ricky <plastic.acolyte.art@gmail.com>
2024-06-19 21:51:48 -0500

    Delete Stack eat functions

--- python/stack.py
+++ python/stack.py
@@ -9,21 +9,3 @@ class Stack:
 
     def push(self, value: int) -> None:
         self.contents.append(value)
-
-    def eat_as_char(self) -> str:
-        result = ""
-
-        for value in self.contents:
-            result += chr(value)
-
-        self.contents = []
-        return result
-
-    def eat_as_num(self) -> int:
-        result = 0
-
-        for value in self.contents:
-            result += value
-
-        self.contents = []
-        return result
