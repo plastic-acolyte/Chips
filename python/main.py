b4067a11baecce09ced2a0c0f12e5c856ef28b86
Ricky <plastic.acolyte.art@gmail.com>
2024-06-30 21:44:35 -0500

    Remove handle functions that really aren't useful

--- python/main.py
+++ python/main.py
@@ -16,32 +16,19 @@ def main():
             params = pieces[1] if len(pieces) > 1 else None
 
             match cmd:
-                case Token.ADD: handle_add(chips, params)
-                case Token.BANK: handle_bank(chips, params)
+                case Token.ADD: chips.add()
+                case Token.BANK: chips.bank()
                 case Token.DRAW: handle_draw(chips, params)
-                case Token.EAT: handle_eat(chips, params)
-                case Token.NEXT: handle_pop(chips, params)
-                case Token.POP: handle_pop(chips, params)
-                case Token.PREV: handle_pop(chips, params)
-                case Token.PUSH: handle_push(chips, params)
-                case Token.SUB: handle_sub(chips, params)
-                case Token.SWAP: handle_swap(chips, params)
-                case Token.WAGER: handle_wager(chips, params)
+                case Token.EAT: chips.eat()
+                case Token.FLIP: chips.flip()
+                case Token.NEXT: chips.next()
+                case Token.POP: chips.pop()
+                case Token.PREV: chips.prev()
+                case Token.PUSH: chips.push()
+                case Token.SUB: chips.sub()
+                case Token.SWAP: chips.swap()
+                case Token.WAGER: chips.wager()
                 case _: raise ValueError
-
-
-def handle_add(chips, params):
-    if params:
-        raise ValueError
-
-    chips.add()
-
-
-def handle_bank(chips, params):
-    if params:
-        raise ValueError
-
-    chips.bank()
 
 
 def handle_draw(chips, params):
@@ -52,69 +39,6 @@ def handle_draw(chips, params):
         raise ValueError
 
     chips.draw(int(params))
-
-
-def handle_eat(chips, params):
-    if params:
-        raise ValueError
-
-    chips.eat()
-
-
-def handle_flip(chips, params):
-    if params:
-        raise ValueError
-
-    chips.flip()
-
-
-def handle_next(chips, params):
-    if params:
-        raise ValueError
-
-    chips.next()
-
-
-def handle_pop(chips, params):
-    if params:
-        raise ValueError
-
-    chips.pop()
-
-
-def handle_prev(chips, params):
-    if params:
-        raise ValueError
-
-    chips.prev()
-
-
-def handle_push(chips, params):
-    if params:
-        raise ValueError
-
-    chips.push()
-
-
-def handle_sub(chips, params):
-    if params:
-        raise ValueError
-
-    chips.sub()
-
-
-def handle_swap(chips, params):
-    if params:
-        raise ValueError
-
-    chips.swap()
-
-
-def handle_wager(chips, params):
-    if params:
-        raise ValueError
-
-    chips.wager()
 
 
 if __name__ == '__main__':
