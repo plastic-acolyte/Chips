e2002032f84bc5973667542b57ceeb3e2b9d7b49
Ricky <plastic.acolyte.art@gmail.com>
2024-06-30 23:49:51 -0500

    use reader class to read tokens

--- python/main.py
+++ python/main.py
@@ -4,41 +4,33 @@ from chips import Chips
 
 
 def main():
+    reader = Reader(argv[1])
     chips = Chips()
 
-    with open(argv[1]) as file:
-        for line in file:
-            if line[0] == '\n' or line[0] == '#':
-                continue
+    while reader.has_token():
+        token = reader.get_token()
 
-            pieces = line.strip().split(maxsplit=1)
-            cmd = Token.parse(pieces[0])
-            params = pieces[1] if len(pieces) > 1 else None
+        match token[0]:
+            # Parameterless Commands
+            case Token.ADD: chips.add()
+            case Token.BANK: chips.bank()
+            case Token.EAT: chips.eat()
+            case Token.FLIP: chips.flip()
+            case Token.NEXT: chips.next()
+            case Token.POP: chips.pop()
+            case Token.PREV: chips.prev()
+            case Token.PUSH: chips.push()
+            case Token.SUB: chips.sub()
+            case Token.SWAP: chips.swap()
+            case Token.WAGER: chips.wager()
 
-            match cmd:
-                case Token.ADD: chips.add()
-                case Token.BANK: chips.bank()
-                case Token.DRAW: handle_draw(chips, params)
-                case Token.EAT: chips.eat()
-                case Token.FLIP: chips.flip()
-                case Token.NEXT: chips.next()
-                case Token.POP: chips.pop()
-                case Token.PREV: chips.prev()
-                case Token.PUSH: chips.push()
-                case Token.SUB: chips.sub()
-                case Token.SWAP: chips.swap()
-                case Token.WAGER: chips.wager()
-                case _: raise ValueError
+            # Commands with one parameter
+            case Token.DRAW: handle_draw(chips, reader)
 
 
-def handle_draw(chips, params):
-    if not params:
-        raise ValueError
-
-    elif not params.isnumeric():
-        raise ValueError
-
-    chips.draw(int(params))
+def handle_draw(chips, reader):
+    token = reader.get_token()
+    chips.draw(token[1])
 
 
 if __name__ == '__main__':
